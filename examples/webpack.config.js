ee4222cffe75099dfc9de63532a230cf57480947
Max Rolon <max.rolon@barrelny.com>
2017-10-31 11:20:17 -0400

    Cache busts when new assets are compiled

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist/assets'),
     filename: 'main.js',
-    chunkFilename: "[name]-[id].js"
+    chunkFilename: `[name]-[id].js?version=${Date.now()}`
   },
   module: {
     rules: [
